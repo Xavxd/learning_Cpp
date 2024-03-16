fed6b3b9c287fb6ea3378752342da16ab6385259
Xavpunkxd <jon20xavi@gmail.com>
2024-03-16 14:30:20 -0600

    modificando alumno.exe y terminando el programa para hacer operaciones con numeros complejos

--- Numeros_Complejos_Cpp/Complejo.h
+++ Numeros_Complejos_Cpp/Complejo.h
@@ -1,9 +1,13 @@
 #ifndef COMPLEJO
 #define COMPLEJO
-
+#include <cmath>
+#include <string>
+#include <sstream>
+#include<iomanip>
+using namespace std;
 class Complejo
 {
-private:
+private: 
     /* data */
     double real,imag;
 public:
@@ -14,16 +18,45 @@ public:
     //getters
     double get_real();
     double get_imag();
+    string get_complejo();
 
     //methods
     Complejo suma(Complejo z_1,Complejo z_2);
-    /*Complejo resta(Complejo z_1,Complejo z_2);
+    Complejo resta(Complejo z_1,Complejo z_2);
     Complejo multiplicacion(Complejo z_1,Complejo z_2);
-    Complejo divicion(Complejo z_1,Complejo z_2);*/    
+    Complejo divicion(Complejo z_1,Complejo z_2);
 };
 
 Complejo Complejo::suma(Complejo z_1,Complejo z_2){
-    //real = ;
+    real = z_1.get_real() + z_2.get_real();
+    imag = z_1.get_imag() + z_2.get_imag();
+
+    return Complejo(real,imag);
+}
+
+Complejo Complejo::resta(Complejo z_1,Complejo z_2){
+    real = z_1.get_real() - z_2.get_real();
+    imag = z_1.get_imag() - z_2.get_imag();
+
+    return Complejo(real,imag);
+}
+
+Complejo Complejo::multiplicacion(Complejo z_1,Complejo z_2){
+    double a=z_1.get_real(),b=z_1.get_imag();//obteniendo los numeros de z_1
+    double c=z_2.get_real(),d=z_2.get_imag();//obteniendo los numeros de z_2
+    real= (a*c)+(b*d*(-1)) ;
+    imag= (b*c)+(a*d) ;
+
+    return Complejo(real,imag);
+}
+
+Complejo Complejo::divicion(Complejo z_1,Complejo z_2){
+    double a=z_1.get_real(), b=z_1.get_imag();//obteniendo los numeros de z_1
+    double c=z_2.get_real(), d=(z_2.get_imag())*(-1);//obteniendo los numeros de z_2
+    real= ((a*c)+(b*d*(-1))) / (pow(c,2) + pow(d,2));
+    imag= ((b*c)+(a*d)) / (pow(c,2) + pow(d,2));
+
+    return Complejo(real,imag);
 }
 
 Complejo::Complejo(double real,double imag){
@@ -44,5 +77,13 @@ double Complejo::get_imag(){
     return imag;
 }
 
+string Complejo::get_complejo(){
+    stringstream str,stimg;
+    //dandole formato a los numeros 
+    str<< fixed<< setprecision(2)<<real;
+    stimg<< fixed<< setprecision(2)<<imag;
+    string s= "("+ str.str() +","+ stimg.str() +")";
+    return s;
+}
 
 #endif
--- Numeros_Complejos_Cpp/main.cpp
+++ Numeros_Complejos_Cpp/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-//#include ""
+#include "Complejo.h"
 
+using namespace std;
 int main(){
-    return 0;
-}
+    //declarando numeros
+    Complejo z_1(9,-4);
+    Complejo z_2(8,5);
+
+    //Operaciones:
+    Complejo s=s.suma( z_1,z_2 );
+    Complejo r=r.resta(z_1,z_2);
+    Complejo multi=multi.multiplicacion(z_1,z_2);
+    Complejo divic=divic.divicion(z_1,z_2);
+    
+   cout<<"suma= "<< s.get_complejo() <<"\n";
+   cout<<"resta= "<< r.get_complejo() <<"\n";
+   cout<<"multiplicacion= "<< multi.get_complejo() <<"\n";
+   cout<<"divicion= "<< divic.get_complejo() <<"\n";
+} 
